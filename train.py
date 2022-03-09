b1190f17b69f5deb76cc6ec4c753a635b11a2b46
Mahan Fathi <mahan0fathi@gmail.com>
2022-03-09 11:43:33 -0500

    Add model to train outputs

--- train.py
+++ train.py
@@ -401,7 +401,7 @@ def train(
 
     pmap.synchronize_hosts()
 
-    return (inference_fn, policy_model, params, metrics)
+    return (inference_fn, params, metrics, (obs_normalizer_apply_fn, policy_model, parametric_action_distribution))
 
 
 def make_inference_fn(cfg, core_env, dt, normalize_observations):
